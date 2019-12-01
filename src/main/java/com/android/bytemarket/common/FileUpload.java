0cde6f82662a6f2b0cd88e1e1ff2c9fe48fa9268
lequal <yikeliang@outlook.com>
2019-12-01 21:24:28 +0800

    加入文件上传

--- src/main/java/com/android/bytemarket/common/FileUpload.java
+++ src/main/java/com/android/bytemarket/common/FileUpload.java
@@ -1,46 +1,69 @@
 package com.android.bytemarket.common;
 
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
 import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
 import com.qiniu.util.StringMap;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author: 15760
  * @Date: 2019/12/1
  * @Descripe:
  */
+@Component
 public class FileUpload {
 
-    static String accessKey = "6sLzFMI1OuMEabWiej9QZ7p1NRK0OYR5tv808xVn";
-    static String secretKey = "lmr9I8cQVlPEL8YVFNFkPVqCnkOZ--CHWBNxvj9X";
+    static String accessKey = "";
+    static String secretKey = "";
     //需要上传的空间名
-    static String bucket = "bucket name";
-    static String filePath = "";
-    static String key = "file key";
+    static String bucket = "yoyoyo";
 
-    //图片文件后缀名
-    //static final String[] fileSuffix = {"jpg", "jpeg", "png"};
+    static String key = null;
 
     //密钥配置
-    static Auth auth = Auth.create(accessKey, secretKey);
+    //static Auth auth = Auth.create(accessKey, secretKey);
     //创建上传对象
-    static UploadManager uploadManager = new UploadManager(new Configuration());
-
-    //简单上传，使用默认策略，只需要设置上传的空间名即可
-    public static String simpleUpload() {
-        return auth.uploadToken(bucket);
-    }
+    static UploadManager uploadManager = new UploadManager(new Configuration(Region.autoRegion()));
 
     /**
      * @param file
      * @return
      */
     public static String upLoad(MultipartFile file) throws IOException {
-
+        try {
+            byte[] uploadBytes = file.getBytes();
+            ByteArrayInputStream byteInputStream=new ByteArrayInputStream(uploadBytes);
+            Auth auth = Auth.create(accessKey, secretKey);
+            String upToken = auth.uploadToken(bucket);
+            try {
+                Response response = uploadManager.put(byteInputStream,key,upToken,null, null);
+                //解析上传成功的结果
+                DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+                System.out.println(putRet.key);
+                System.out.println(putRet.hash);
+            } catch (QiniuException ex) {
+                Response r = ex.response;
+                System.err.println(r.toString());
+                try {
+                    System.err.println(r.bodyString());
+                } catch (QiniuException ex2) {
+                    //ignore
+                }
+            }
+        } catch (UnsupportedEncodingException ex) {
+            //ignore
+        }
         return "";
     }
 }
