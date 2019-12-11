16c99a88dee698c041fe3fe3c4ca633c66fb1d04
PinTeh <794409767@qq.com>
2019-12-11 21:00:38 +0800

    添加获取商品详情接口

--- src/main/java/com/android/bytemarket/controller/ProductController.java
+++ src/main/java/com/android/bytemarket/controller/ProductController.java
@@ -13,11 +13,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -35,6 +31,26 @@ public class ProductController {
     private ProductService productService;
     @Autowired
     private UserService userService;
+
+    @GetMapping("/{id}")
+    public ServerResponse get(@PathVariable("id") Integer id){
+        Product product = productService.getById(id);
+        ProductResponse productResponse = getProductResponse(product);
+        return ServerResponse.ofSuccess(productResponse);
+    }
+
+    private ProductResponse getProductResponse(Product product) {
+        ProductResponse productResponse = new ModelMapper().map(product, ProductResponse.class);
+        User u = userService.getById(productResponse.getUserId());
+        if (u != null) {
+            ProductResponse.User pu = new ProductResponse.User();
+            pu.setId(u.getId());
+            pu.setName(u.getNickName());
+            pu.setAvatar(u.getAvatar());
+            productResponse.setUser(pu);
+        }
+        return productResponse;
+    }
 
     @GetMapping("/list")
     public ServerResponse list(@RequestParam(defaultValue = "1")Integer page, @RequestParam(defaultValue = "10")Integer limit,@RequestParam(required = false)Integer cid){
@@ -66,15 +82,7 @@ public class ProductController {
     private List<ProductResponse> packageResponse(List<Product> products){
         List<ProductResponse> list = new LinkedList<>();
         products.forEach(v->{
-            ProductResponse productResponse = new ModelMapper().map(v,ProductResponse.class);
-            User u = userService.getById(productResponse.getUserId());
-            if (u!=null){
-                ProductResponse.User pu = new ProductResponse.User();
-                pu.setId(u.getId());
-                pu.setName(u.getNickName());
-                pu.setAvatar(u.getAvatar());
-                productResponse.setUser(pu);
-            }
+            ProductResponse productResponse = getProductResponse(v);
             list.add(productResponse);
         });
         return list;
