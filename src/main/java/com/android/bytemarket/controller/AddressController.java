5e00df6ad80e65bea7095a3ef01fbde6231b2ee1
PinTeh <794409767@qq.com>
2019-12-16 16:14:40 +0800

    新增收获地址模块

---
+++ src/main/java/com/android/bytemarket/controller/AddressController.java
@@ -0,0 +1,36 @@
+package com.android.bytemarket.controller;
+
+import com.android.bytemarket.common.ServerResponse;
+import com.android.bytemarket.entity.Address;
+import com.android.bytemarket.service.AddressService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping("/list")
+    public ServerResponse list(@RequestParam("uid")Integer uid){
+        //无校验
+        List<Address> list =addressService.list(new QueryWrapper<Address>().eq("user_id",uid));
+        return ServerResponse.ofSuccess(list);
+    }
+
+    @PostMapping("/")
+    public ServerResponse save(@RequestBody Address address){
+        boolean b = addressService.saveOrUpdate(address);
+        if (b) {
+            return ServerResponse.ofSuccess(address);
+        }
+        else {
+            return ServerResponse.ofError();
+        }
+    }
+}
---
+++ src/main/java/com/android/bytemarket/service/AddressService.java
@@ -0,0 +1,7 @@
+package com.android.bytemarket.service;
+
+import com.android.bytemarket.entity.Address;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface AddressService extends IService<Address> {
+}
